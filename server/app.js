17cd1c862da429f937ab6262b3aa5507e1110759
JaideepSingh99 <jai46560@gmail.com>
2025-06-10 02:18:38 +0530

    Setup MongoDB connection with environment config

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
+import {PORT} from "./config/env.js";
+import connectToDatabase from "./database/mongodb.js";
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +14,10 @@ app.get('/', (req, res) => {
     res.send("Server is running");
 });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
+
+export default app;
